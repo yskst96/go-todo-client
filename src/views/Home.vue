392bf23f48722fa67bfdda4f7a0f8c70765e17e2
yusukesata <yskstdev@gmail.com>
2020-05-28 01:56:43 +0900

    3.0向けに修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default defineComponent({});
 </script>
